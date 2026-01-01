198373dc78be3d1bed2df667eaa6fdd034fbd7d3
94kp <karan94patil@gmail.com>
2026-01-01 10:51:51 -0500

    added robot control and sim environment

---
+++ build/my_bot/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_828f1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_828f1.dir/CMakeCXXCompilerABI.cpp.o"
+  "cmTC_828f1"
+  "cmTC_828f1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_828f1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
